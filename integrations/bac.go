fe7a91853be1a93b8163d48d2977a0529a4cb2e3
Diego <pcdiego17@gmail.com>
2026-04-07 10:40:19 -0600

    adding logs to try env file

--- integrations/bac.go
+++ integrations/bac.go
@@ -117,7 +117,7 @@ func FetchBAC() ([]models.PromocionUnificada, error) {
 				FechaInicio:         doc.ValidityFrom,
 				FechaFin:            doc.ValidityTo,
 				PorcentajeDescuento: doc.DiscountPercentValue,
-				UrlExterna:          "https://mipromo.com/sv/deal/" + doc.id ,
+				UrlExterna:          "https://mipromo.com/sv/deal/" + doc.Id ,
 			})
 		}
 		start += limit
